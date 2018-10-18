3eb8cb1b33ac387b766cd6b3d81995353494589f
BK_Mehta <bkmehtacool@gmail.com>
2018-10-18 13:58:05 +0530

    initial commit

---
+++ models/product.js
@@ -0,0 +1,19 @@
+// var mongoose = require('mongoose');
+// var Schema = mongoose.Schema;
+//
+// var schema = new schema({
+//      imagePath: {
+//           type: String,
+//           required: true
+//      },
+//      title: {
+//           type: String,
+//           required: true
+//      },
+//      price: {
+//           type: Number,
+//           required: true
+//      }
+// });
+//
+// model.exports = mongoose.model('Product',schema);
---
+++ seed/product-seeder.js
@@ -0,0 +1,9 @@
+// var product = require('../models/product');
+//
+// var products = [
+//      new Product({
+//           imagePath: '',
+//           title: 'Gothic Video Game',
+//           description: 'aw'
+//      }),
+// ];
